a303d1c9ac0300f449c1f60496bba8dab2d1d60f
Chris <Chris@Cloudyde-MacBook-Pro.local>
2021-10-15 15:53:17 +0800

    20211015

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use("/api/user", memberRoutes);
 app.use("/api/articles", articleRoutes);
 
 // home route
-app.use((req, res, next) => res.send("Hi welcome to wow-gym API server 👻!"));
+// app.use((req, res, next) => res.send("Hi welcome to wow-gym API server 👻!"));
 app.use('/',express.static('./build'));
 // Error handler
 app.use((req, res, next) => {
