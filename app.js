fd2819b69d1ecd996301650bedb8a11840b24aa4
Chris <Chris@Cloudyde-MacBook-Pro.local>
2021-10-15 16:11:47 +0800

    20211015

--- app.js
+++ app.js
@@ -72,10 +72,10 @@ app.use("/api/articles", articleRoutes);
 // app.use((req, res, next) => res.send("Hi welcome to wow-gym API server 👻!"));
 app.use('/',express.static('./build'));
 // Error handler
-app.use((req, res, next) => {
-  throw new httpError("Route can't find!", 404);
-});
-
+// app.use((req, res, next) => {
+//   throw new httpError("Route can't find!", 404);
+// });
+app.use((req, res, next) => res.send("can't find data 👻!"));
 
 app.use((error, req, res, next) => {
   if (res.headerSent) return next(error);
