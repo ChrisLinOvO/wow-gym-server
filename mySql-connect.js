995df375b9d2ee5bb95d63e4f2dacfaf84ef25ae
Chris <Chris@Cloudyde-MacBook-Pro.local>
2021-10-15 14:08:35 +0800

    20211015

--- mySql-connect.js
+++ mySql-connect.js
@@ -1,25 +1,24 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "mydb",
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0,
-});
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "12345",
+//   database: "mydb",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 // production config
 
-// const pool = mysql.createPool({
-//   host: "eu-cdbr-west-03.cleardb.net",
-//   user: "b7aafd89bf2851",
-//   password: "633fdffc",
-//   database: "heroku_a97c1aaa6daa789",
-// });
+const pool = mysql.createPool({
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b42f4a7274b8ff",
+  password: "3c3c37ed",
+  database: "heroku_28e16c61fcb839f",
+});
 
 module.exports = pool.promise();
 
 
 
-// -------
