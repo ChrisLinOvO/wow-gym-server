3da63d99bc5b59a209fa72873f98b4f7d3564547
Chris Lin <chrislin@ezcon.com>
2023-11-06 10:59:16 +0800

    update db

--- mySql-connect.js
+++ mySql-connect.js
@@ -1,26 +1,26 @@
 const mysql = require("mysql2");
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "wow-gym-v2",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wow-gym-v2",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 // 創建雲端DB參考：
 // https://www.youtube.com/watch?v=RTMCD-oIpz8&t=9s
 // https://www.youtube.com/watch?v=CXiT-DWn1zs&list=PLmuGl8O_czsbymkUbE92njOUHtnMGSOFC&index=26
 
 // production config
-const pool = mysql.createPool({
-  host: "b5ps8x6brydtzteexohi-mysql.services.clever-cloud.com",
-  user: "u2dcit2jejtu14pt",
-  password: "sq3z60coqbG8wdZFujBT",
-  database: "b5ps8x6brydtzteexohi",
-});
+// const pool = mysql.createPool({
+//   host: "b5ps8x6brydtzteexohi-mysql.services.clever-cloud.com",
+//   user: "u2dcit2jejtu14pt",
+//   password: "sq3z60coqbG8wdZFujBT",
+//   database: "b5ps8x6brydtzteexohi",
+// });
 
 module.exports = pool.promise();
 
